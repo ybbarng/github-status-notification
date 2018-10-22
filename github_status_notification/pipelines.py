9cf01c27e1a1b0bcd300529a3e9b15646c06957d
ybbarng <ybbarng@gmail.com>
2018-10-22 16:14:34 +0900

    implement github status messages

--- github_status_notification/pipelines.py
+++ github_status_notification/pipelines.py
@@ -4,8 +4,71 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+
+from scrapy.exceptions import DropItem
 
 
-class GithubStatusNotificationPipeline(object):
+DATABASE = 'messages.jl'
+KEY_TIMESTAMP = 't'
+KEY_STATUS = 's'
+
+
+class DataTidyPipeline(object):
     def process_item(self, item, spider):
+        if item['status'] is None:
+            item['status'] = 'good'
         return item
+
+
+class NewMessagePipeline(object):
+
+    def open_spider(self, spider):
+        self.notified = set()
+        spider.latest_status = 'good'
+        try:
+            with open(DATABASE, 'r') as f:
+                for line in f:
+                    log = json.loads(line)
+                    self.notified.add(log[KEY_TIMESTAMP])
+                    spider.latest_status = log[KEY_STATUS]
+        except FileNotFoundError:
+            pass
+
+    def process_item(self, item, spider):
+        if item['timestamp'] in self.notified:
+            raise DropItem('Already notified')
+        self.notified.add(item['timestamp'])
+
+        return item
+
+
+class JsonWriterPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = open(DATABASE, 'a+')
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        line = json.dumps({KEY_TIMESTAMP: item['timestamp'], KEY_STATUS: item['status']}) + '\n'
+        self.file.write(line)
+        return item
+
+
+class NotifiablePipeline(object):
+
+    def process_item(self, item, spider):
+        if item['status'] == spider.latest_status == 'good':
+            raise DropItem('Stable status')
+        spider.latest_status = item['status']
+        return item
+
+
+class SlackPipeline(object):
+
+    def process_item(self, item, spider):
+        print('Send to slack : {} {} {}'.format(item['timestamp'], item['status'], item['text']))
+        return item
+
---
+++ github_status_notification/spiders/github_status_spider.py
@@ -0,0 +1,35 @@
+import scrapy
+
+class GithubStatusSpider(scrapy.Spider):
+    name = 'github-status'
+
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'github_status_notification.pipelines.DataTidyPipeline': 1,
+            'github_status_notification.pipelines.NewMessagePipeline': 2,
+            'github_status_notification.pipelines.JsonWriterPipeline': 3,
+            'github_status_notification.pipelines.NotifiablePipeline': 4,
+            'github_status_notification.pipelines.SlackPipeline': 5,
+        }
+    }
+
+    latest_status = None
+
+    def start_requests(self):
+        urls = ['https://status.github.com/messages']
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for message in reversed(response.css('.message')):
+            status = message.css('::attr(data-status)').extract_first()
+            if status is None:
+                status = 'good'
+            timestamp = message.css('time::attr(datetime)').extract_first()
+            text = message.css('span::text').extract_first()
+            yield {
+                'timestamp': timestamp,
+                'status': status,
+                'text': text,
+            }
